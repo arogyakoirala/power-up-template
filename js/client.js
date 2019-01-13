72154b30303358a395a24fa5cef3e1d105a7f75b
Arogya Koirala <arogyakoirala@gmail.com>
2019-01-13 22:11:29 +0545

    new2

--- js/client.js
+++ js/client.js
@@ -282,11 +282,11 @@ TrelloPowerUp.initialize({
   'card-buttons': function(t, options){
       return [{
         icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-        text: 'Estimate Size',
+        text: 'Vote for feature',
         callback: function(t){
           return t.popup({
-            title: "Priority",
-            url: 'estimate.html'
+            title: "How important is this?",
+            url: 'vote.html'
           });
         }
       }];
