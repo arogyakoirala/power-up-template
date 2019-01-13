14d52e8b42c6f69e499903d60e8dd085265484f2
Arogya Koirala <arogyakoirala@gmail.com>
2019-01-13 21:55:06 +0545

    initial commit

--- js/vote.js
+++ js/vote.js
@@ -5,6 +5,7 @@ window.estimate.addEventListener('submit', function(event){
   event.preventDefault();
   return t.set('card', 'shared', 'vote', window.votePriority.value)
   .then(function(){
+    console.log('value set to ', window.votePriority.value);
     t.closePopup();
   });
 });
