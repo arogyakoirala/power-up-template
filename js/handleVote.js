bc72867daef2c8f615b5e7ebbcff467dcef2bd03
Arogya Koirala <arogyakoirala@gmail.com>
2019-01-13 23:18:55 +0545

    new22aas

--- js/handleVote.js
+++ js/handleVote.js
@@ -2,16 +2,16 @@ var t = TrelloPowerUp.iframe();
 
 window.vote.addEventListener('submit', function(event){
 
-  console.log(t.get('card', 'shared', 'nice_to_have'), 'Hereiswhat')
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
   return t.set('member', 'shared', 'vote', window.votePriority.value)
   .then(function(){
+    t.set('card', 'shared', {nice_to_have:0, important:0, critical:0})
   }).then(function(){
     return t.getAll()
   }).then(function(res){
     console.log('Myserponse, ', res);
-    return t.set('card', 'shared', window.votePriority.value, res+1);
+    return t.set('card', 'shared', window.votePriority.value, res.card.shared[window.votePriority.value]+1);
   }).then(function(){
     t.closePopup();
   });
