16d1e6adad074a732b4767a74aad352994b13186
Arogya Koirala <arogyakoirala@gmail.com>
2019-01-15 02:39:53 +0545

    member stuff for fun

--- js/client.js
+++ js/client.js
@@ -212,23 +212,15 @@ TrelloPowerUp.initialize({
   },
       "card-badges": function(t, opts){
         return t.getAll().then(function(allValues){
-          var nthtext= (allValues.card && allValues.card.shared && allValues.card.shared.nth) ? "Nice to have: "+ String(allValues.card.shared.nth): "Nice to have: 0";
-          var imptext= (allValues.card && allValues.card.shared && allValues.card.shared.imp )? "Important: "+ String(allValues.card.shared.imp): "Important: 0";
-          var criticaltext= (allValues.card && allValues.card.shared && allValues.card.shared.cri) ? "Critical: "+ String(allValues.card.shared.cri): "Critical: 0";
+          var selection = allValues.card.private.selected || 'unspecified';
+          var counts = allValues.card.shared.nth + allValues.card.shared.imp + allValues.card.shared.cri || 0
 
           return [
             {
-              text: nthtext,
-              color:'yellow',
-            },
-            {
-              text: imptext,
+              text: textMapper[selection],
               color:'green',
             },
-            {
-              text: criticaltext,
-              color:'red',
-            },
+
         ]
         })
       }
