82adf4ac3a4ff2a6bfdd15bd1c0b69d6dbe4bcbc
Arogya Koirala <arogyakoirala@gmail.com>
2019-01-14 14:51:07 +0545

    test 334

--- js/client.js
+++ js/client.js
@@ -121,6 +121,7 @@ TrelloPowerUp.initialize({
   },
     "card-badges": function(t, opts){
       return t.getAll().then(function(allValues){
+        console.log(allValues);
         var nthtext= (allValues.card && allValues.card.shared && allValues.card.shared.nth) ? "Nice to have: "+ String(allValues.card.shared.nth): "Nice to have: 0";
         var imptext= (allValues.card && allValues.card.shared && allValues.card.shared.imp )? "Important: "+ String(allValues.card.shared.imp): "Important: 0";
         var criticaltext= (allValues.card && allValues.card.shared && allValues.card.shared.cri) ? "Critical: "+ String(allValues.card.shared.cri): "Critical: 0";
