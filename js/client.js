7630ae3cbf46399408316d79249cec892403bb0e
Arogya Koirala <arogyakoirala@gmail.com>
2019-01-14 21:27:47 +0545

    member stuff for fun

--- js/client.js
+++ js/client.js
@@ -34,11 +34,10 @@ TrelloPowerUp.initialize({
                   return t.getAll();
                 })
                 .then(function(data){
-                  console.log(data, data.card.private.memberId, data.card.shared && data.card.shared.votingMembers && !data.card.shared.votingMembers.includes(data.card.private.memberId))
 
                   var array = [];
                   return t.set('card', 'shared', {
-                    votingMembers: data.card.shared && data.card.shared.votingMembers && !data.card.shared.votingMembers.includes(data.card.private.memberId) && data.card.shared.votingMembers.push(data.card.private.memberId) || array.push[memberId],
+                    votingMembers: data.card.shared && data.card.shared.votingMembers && !data.card.shared.votingMembers.includes(data.card.private.memberId) && data.card.shared.votingMembers.push(data.card.private.memberId) || array.push(data.card.private.memberId),
                   })
                 })
                 .then(function(){
