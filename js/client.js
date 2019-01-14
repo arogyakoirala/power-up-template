41b70400ae756980da301149f4b273c7b23b1fa8
Arogya Koirala <arogyakoirala@gmail.com>
2019-01-14 09:19:40 +0545

    ggff

--- js/client.js
+++ js/client.js
@@ -1,10 +1,15 @@
 TrelloPowerUp.initialize({
   'card-detail-badges': function(t, opts) {
-
+    return t.set('card', 'shared', {currentSelection: 'none', pastSelection: 'none','nice_to_have':0, 'important':0, 'critical':0})
+    .then(function(){
+      return t.get('card', 'shared', currentSelection)
+    }).then(function(res){
       return [{
         title: 'Set Priority',
-        text:'My priority',
+        text:'Test',
         color:null
       }]
+    })
   }
-});
+
+})
