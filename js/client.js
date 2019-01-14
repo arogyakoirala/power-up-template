ac79fdf84d7d04de0afe1cdc781dd324e1f131a6
Arogya Koirala <arogyakoirala@gmail.com>
2019-01-14 09:38:33 +0545

    refactored client.js

--- js/client.js
+++ js/client.js
@@ -7,9 +7,64 @@ TrelloPowerUp.initialize({
       return [{
         title: 'Set Priority',
         text:res,
-        color:null
+        color:null,
+        callback: function(t, opts) {
+          return t.popup({
+              title: 'Set priority',
+              items: [{
+                text: 'Nice to have',
+                callback: function (t, opts) {
+                  t.getAll().then(fuction(res){
+                    t.set('card', 'shared', {
+                      currentSelection:'nice_to_have',
+                      pastSelection: res.card.shared.currentSelection,
+                      nice_to_have: Number(res)+1,
+                      [res.card.shared.currentSelection]: Number(res.card.shared.currentSelection)-1
+                    }
+                  ).then(function(){
+                      t.closePopup();
+                    })
+                  })
+                }
+              }, {
+                text: 'Important',
+                callback: function (t, opts) {
+                  t.getAll().then(fuction(res){
+                    t.set('card', 'shared', {
+                      currentSelection:'important',
+                      pastSelection: res.card.shared.currentSelection,
+                      important: Number(res)+1,
+                      [res.card.shared.currentSelection]: Number(res.card.shared.currentSelection)-1 
+                    }
+                  ).then(function(){
+                      t.closePopup();
+                    })
+                  })
+
+                }
+              }, {
+                text: 'Critical',
+                callback: function (t, opts) {
+                  t.getAll().then(fuction(res){
+                    t.set('card', 'shared', {
+                      currentSelection:'critical',
+                      pastSelection: res.card.shared.currentSelection,
+                      critical: Number(res)+1,
+                      [res.card.shared.currentSelection]: Number(res.card.shared.currentSelection)-1 
+                    }
+                  ).then(function(){
+                      t.closePopup();
+                    })
+                  })
+                }
+              }]
+            });
+        }
       }]
     })
   }
 
 })
+
+
+const
